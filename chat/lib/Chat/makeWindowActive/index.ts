60ffe77b7cfcfa82f80eac1b5bafe864c80f3731
Harsh Lodhi <harshlodhi82@gmail.com>
2020-04-17 22:00:56 +0530

    Added quats to the window name

--- chat/lib/Chat/makeWindowActive/index.ts
+++ chat/lib/Chat/makeWindowActive/index.ts
@@ -2,7 +2,7 @@ import child_process from 'child_process'
 import log from 'lib/utils/logger'
 
 const makeWindowActive = function () {
-  const command = `nircmd win activate ititle ${this.windowName} && nircmd win togglemin ititle ${this.windowName} && nircmd win togglemax ititle ${this.windowName}`
+  const command = `nircmd win activate ititle "${this.windowName}" && nircmd win togglemin ititle "${this.windowName}" && nircmd win togglemax ititle "${this.windowName}"`
   
   this.activeWindowInterval = setInterval(function () {
     let process = child_process.exec(command)
